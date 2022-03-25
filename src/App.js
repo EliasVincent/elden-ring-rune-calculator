03d80cb101f83eae2b6e63f853c9872ca03851bb
EliasVincent <riesyeti@outlook.de>
2022-03-25 15:51:52 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import RuneTypes, { goldenRune1 } from "./RuneTypes";
 
 function App() {
+  // state number
+  const [runes, setRunes] = React.useState(0);
+
+  const addRunes = (runesToAdd) => {
+    setRunes(runes + runesToAdd);
+  };
+
+  const subtractRunes = (runesToSubtract) => {
+    setRunes(runes - runesToSubtract);
+  };
+
+  const [goldenRune1Counter, setGoldenRune1Counter] = React.useState(0);
+  const [goldenRune2Counter, setGoldenRune2Counter] = React.useState(0);
+  const [goldenRune3Counter, setGoldenRune3Counter] = React.useState(0);
+  const [goldenRune4Counter, setGoldenRune4Counter] = React.useState(0);
+  const [goldenRune5Counter, setGoldenRune5Counter] = React.useState(0);
+  const [goldenRune6Counter, setGoldenRune6Counter] = React.useState(0);
+  const [goldenRune7Counter, setGoldenRune7Counter] = React.useState(0);
+  const [goldenRune8Counter, setGoldenRune8Counter] = React.useState(0);
+  const [goldenRune9Counter, setGoldenRune9Counter] = React.useState(0);
+  const [goldenRune10Counter, setGoldenRune10Counter] = React.useState(0);
+  const [goldenRune11Counter, setGoldenRune11Counter] = React.useState(0);
+  const [goldenRune12Counter, setGoldenRune12Counter] = React.useState(0);
+  const [goldenRune13Counter, setGoldenRune13Counter] = React.useState(0);
+  const [numensRuneCounter, setNumensRuneCounter] = React.useState(0);
+  const [herosRune1Counter, setHerosRune1Counter] = React.useState(0);
+  const [herosRune2Counter, setHerosRune2Counter] = React.useState(0);
+  const [herosRune3Counter, setHerosRune3Counter] = React.useState(0);
+  const [herosRune4Counter, setHerosRune4Counter] = React.useState(0);
+  const [herosRune5Counter, setHerosRune5Counter] = React.useState(0);
+  const [lordsRuneCounter, setLordsRuneCounter] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Elden Ring Rune Calculator</h1>
+
+      <div className="RuneContainer">
+        <div>Golden Rune 1</div>
+        <div className="RuneButtonContainer">
+          <button
+            onClick={() => {
+              addRunes(goldenRune1);
+              setGoldenRune1Counter(goldenRune1Counter + 1);
+            }}
+          >
+            +
+          </button>
+          <div>{goldenRune1Counter}</div>
+          <button
+            onClick={() => {
+              subtractRunes(goldenRune1);
+              setGoldenRune1Counter(goldenRune1Counter - 1);
+            }}
+          >
+            -
+          </button>
+        </div>
+      </div>
+
+      <h2>Total Runes: {runes}</h2>
     </div>
   );
 }
-
 export default App;
---
+++ src/RuneTypes.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const goldenRune1 = 200;
+export const goldenRune2 = 400;
+export const goldenRune3 = 800;
+export const goldenRune4 = 1200;
+export const goldenRune5 = 1600;
+export const goldenRune6 = 2000;
+export const goldenRune7 = 2500;
+export const goldenRune8 = 3000;
+export const goldenRune9 = 3800;
+export const goldenRune10 = 5000;
+export const goldenRune11 = 6250;
+export const goldenRune12 = 7500;
+export const goldenRune13 = 10000;
+export const numensRune = 12500;
+export const herosRune1 = 15000;
+export const herosRune2 = 20000;
+export const herosRune3 = 25000;
+export const herosRune4 = 30000;
+export const herosRune5 = 35000;
+export const lordsRune = 50000;
+
+export default function RuneTypes() {
+  // all rune types
+
+  return (
+    <div> </div>
+  );
+}
