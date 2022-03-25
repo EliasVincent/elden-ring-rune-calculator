0de8451feab468930e7e12e9e0cceb84c06b843e
EliasVincent <riesyeti@outlook.de>
2022-03-25 17:32:08 +0100

    Some Styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 import React from "react";
+// import images
+import goldenRune1Img from "../src/images/golden_rune_1_elden_ring_wiki_guide_200px.png";
+import goldenRune2Img from "../src/images/golden_rune_2_elden_ring_wiki_guide_200px.png";
+import goldenRune3Img from "../src/images/golden_rune_3_elden_ring_wiki_guide_200px.png";
+import goldenRune4Img from "../src/images/golden_rune_4_elden_ring_wiki_guide_200px.png";
+import goldenRune5Img from "../src/images/golden_rune_5_elden_ring_wiki_guide_200px.png";
+import goldenRune6Img from "../src/images/golden_rune_6_elden_ring_wiki_guide_200px.png";
+import goldenRune7Img from "../src/images/golden_rune_7_elden_ring_wiki_guide_200px.png";
+import goldenRune8Img from "../src/images/golden_rune_8_elden_ring_wiki_guide_200px.png";
+import goldenRune9Img from "../src/images/golden_rune_9_elden_ring_wiki_guide_200px.png";
+import goldenRune10Img from "../src/images/golden_rune_10_elden_ring_wiki_guide_200px.png";
+import goldenRune12Img from "../src/images/golden_rune_12_elden_ring_wiki_guide_200px.png";
+import herosRune1Img from "../src/images/heros-rune-1-consumable-elden-ring-wiki-guide.png"
+import herosRune4Img from "../src/images/heros-rune-4-consumable-elden-ring-wiki-guide.png"
+import herosRune5Img from "../src/images/heros-rune-5-consumable-elden-ring-wiki-guide.png"
+import lordsRuneImg from "../src/images/lords-rune-tools-elden-ring-wiki-guide.png"
 
 const goldenRune1 = 200;
 const goldenRune2 = 400;
@@ -31,6 +47,7 @@ function App() {
   };
 
   const subtractRunes = (runesToSubtract) => {
+    // if runes are less than 0, set to 0
     setRunes(runes - runesToSubtract);
   };
 
@@ -62,6 +79,9 @@ function App() {
       <div className="AllRuneContainers">
         <div className="RuneContainer">
           <div>Golden Rune 1</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune1Img} alt="Golden Rune 1"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -85,6 +105,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 2</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune2Img} alt="Golden Rune 2"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -108,6 +131,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 3</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune3Img} alt="Golden Rune 3"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -131,6 +157,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 4</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune4Img} alt="Golden Rune 4"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -154,6 +183,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 5</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune5Img} alt="Golden Rune 5"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -177,6 +209,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 6</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune6Img} alt="Golden Rune 6"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -200,6 +235,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 7</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune7Img} alt="Golden Rune 7"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -223,6 +261,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 8</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune8Img} alt="Golden Rune 8"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -246,6 +287,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 9</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune9Img} alt="Golden Rune 9"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -269,6 +313,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 10</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune10Img} alt="Golden Rune 10"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -292,6 +339,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 11</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune10Img} alt="Golden Rune 11"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -315,6 +365,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 12</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune12Img} alt="Golden Rune 12"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -338,6 +391,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Golden Rune 13</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune12Img} alt="Golden Rune 13"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -361,6 +417,9 @@ function App() {
 
         <div className="RuneContainer">
           <div>Numen's Rune</div>
+          <div className="RuneImageContainer">
+          <img className="RuneImage" src={goldenRune12Img} alt="Numen's Rune"></img>
+          </div>
           <div className="RuneButtonContainer">
             <button
               onClick={() => {
@@ -384,6 +443,9 @@ function App() {
 
       <div className="RuneContainer">
         <div>Hero's Rune 1</div>
+        <div className="RuneImageContainer">
+        <img className="RuneImage" src={herosRune1Img} alt="Hero's Rune 1"></img>
+        </div>
         <div className="RuneButtonContainer">
           <button
             onClick={() => {
@@ -407,6 +469,9 @@ function App() {
 
       <div className="RuneContainer">
         <div>Hero's Rune 2</div>
+        <div className="RuneImageContainer">
+        <img className="RuneImage" src={herosRune1Img} alt="Hero's Rune 2"></img>
+        </div>
         <div className="RuneButtonContainer">
           <button
             onClick={() => {
@@ -430,6 +495,9 @@ function App() {
 
       <div className="RuneContainer">
         <div>Hero's Rune 3</div>
+        <div className="RuneImageContainer">
+        <img className="RuneImage" src={herosRune1Img} alt="Hero's Rune 3"></img>
+        </div>
         <div className="RuneButtonContainer">
           <button
             onClick={() => {
@@ -453,6 +521,9 @@ function App() {
 
       <div className="RuneContainer">
         <div>Hero's Rune 4</div>
+        <div className="RuneImageContainer">
+        <img className="RuneImage" src={herosRune4Img} alt="Hero's Rune 4"></img>
+        </div>
         <div className="RuneButtonContainer">
           <button
             onClick={() => {
@@ -476,6 +547,9 @@ function App() {
 
       <div className="RuneContainer">
         <div>Hero's Rune 5</div>
+        <div className="RuneImageContainer">
+        <img className="RuneImage" src={herosRune5Img} alt="Hero's Rune 5"></img>
+        </div>
         <div className="RuneButtonContainer">
           <button
             onClick={() => {
@@ -499,6 +573,9 @@ function App() {
 
       <div className="RuneContainer">
         <div>Lord's Rune</div>
+        <div className="RuneImageContainer">
+        <img className="RuneImage" src={lordsRuneImg} alt="Lord's Rune"></img>
+        </div>
         <div className="RuneButtonContainer">
           <button
             onClick={() => {
@@ -522,7 +599,10 @@ function App() {
 
 {/* end of rune section */}
 </div>
+
+      <div className="StickyFooter">
       <h2>Total Runes: {numberWithCommas(runes)}</h2>
+      </div>
     </div>
   );
 }
